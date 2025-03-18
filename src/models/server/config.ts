fb60b4a3fa6303c8ad4cd9cb5c0904f564496d08
Gaurav-04-06 <gauravkohli204@gmail.com>
2025-03-18 11:23:46 +0530

    adding appwrite integration and setup database collections

---
+++ src/models/server/config.ts
@@ -0,0 +1,13 @@
+import env from "@/app/env";
+import { Client, Avatars, Storage, Databases, Users } from "node-appwrite";
+
+const client = new Client()
+  .setEndpoint(env.appwrite.endpoint)
+  .setProject(env.appwrite.projectId);
+
+const avatars = new Avatars(client);
+const storage = new Storage(client);
+const databases = new Databases(client);
+const users = new Users(client);
+
+export { client, avatars, storage, databases, users };
